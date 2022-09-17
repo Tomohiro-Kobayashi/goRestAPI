f4d47c73bd827c7d70b3dd94b3ff7bac8697826d
Tomohiro-Kobayashi <tkobayashi.200@gmail.com>
2022-09-18 00:25:57 +0900

    refactoring http server

--- main.go
+++ main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
-	"os"
+	"log"
+
+	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":10200",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":10200",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-
-	if err != nil {
-		fmt.Printf("failed")
-		os.Exit(1)
 	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("Failed to close: %s", err)
+			return err
+		}
+		return nil
+	})
+
+	<-ctx.Done()
+
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("Failed to shutdown: %s", err)
+	}
+
+	return eg.Wait()
+
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
+	}
+
 }
